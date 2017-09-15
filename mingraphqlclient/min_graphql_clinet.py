94e82d81b1bc3dd2bc51516d1816e82355906625
Nacho Althabe <nacho.althabe@gmail.com>
2017-09-15 00:57:35 -0300

    Fix of missed ssl import

--- mingraphqlclient/min_graphql_clinet.py
+++ mingraphqlclient/min_graphql_clinet.py
@@ -90,6 +90,7 @@ class MinGraphQLClient(object):
         try:
             # handling case for missig https certificate
             if self.endpoint.startswith('https'):
+                import ssl
                 ctx = ssl.create_default_context()
                 ctx.check_hostname = False
                 ctx.verify_mode = ssl.CERT_NONE
